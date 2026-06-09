315fb820f362fa83a6a0484293c343b231265f64
Mathew <yogamathuzz@gamil.com>
2026-06-09 19:05:21 +0530

    feat: implement comparison operators examples

---
+++ operators/comparison_operators.py
@@ -0,0 +1,9 @@
+# Comparison operators: ==, !=, >, <, >=, <=
+a = 10
+b = 5
+print("Equal to:", a == b)  # False
+print("Not equal to:", a != b)  # True
+print("Greater than:", a > b)  # True
+print("Less than:", a < b)  # False
+print("Greater than or equal to:", a >= b)  # True
+print("Less than or equal to:", a <= b)  # False
