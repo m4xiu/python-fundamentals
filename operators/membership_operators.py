7337303ae39922b85907ff5e1680e16d96a34125
Mathew <yogamathuzz@gamil.com>
2026-06-09 19:13:55 +0530

    feat: implement membership operators examples

---
+++ operators/membership_operators.py
@@ -0,0 +1,6 @@
+# Membership Operators: in, not in
+my_list = [1, 2, 3, 4, 5]
+print("Is 3 in the list?", 3 in my_list)  # True
+print("Is 6 in the list?", 6 in my_list)  # False
+print("Is 3 not in the list?", 3 not in my_list)  # False
+print("Is 6 not in the list?", 6 not in my_list)  # True
