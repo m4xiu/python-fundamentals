9ac616f383d0a655fd3c00ca5f9a3379b5ec5593
Mathew <yogamathuzz@gamil.com>
2026-06-10 21:35:24 +0530

    feat: add Python loop examples

---
+++ loops/loops.py
@@ -0,0 +1,14 @@
+# What is mean loops in python?
+# control the flow of code 
+# repeat a block of code over and over unit a condition is met
+# There are two types of loops 
+#   1. For loop
+#   2. While loop
+
+
+for i in range (5):
+    print(i)
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
